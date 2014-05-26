9eef8ea05b98b99a919b49354fd36cba26575aae
Sascha Wolke <git.dev@sascha.wolke.org>
2014-05-26 12:16:04 +0200

    toJson added

--- hadoop/cit-energy-project-hadoop/src/main/java/de/tuberlin/cit/project/energy/hadoop/EnergyBaseDataNodeFilter.java
+++ hadoop/cit-energy-project-hadoop/src/main/java/de/tuberlin/cit/project/energy/hadoop/EnergyBaseDataNodeFilter.java
@@ -1,8 +1,14 @@
 package de.tuberlin.cit.project.energy.hadoop;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hdfs.protocol.LocatedBlocks;
+import org.apache.hadoop.hdfs.web.JsonUtil;
+import org.mortbay.util.ajax.JSON;
 
 
 /**
@@ -23,10 +29,24 @@ public class EnergyBaseDataNodeFilter {
 				+ this.dataNodeSelectorAddress + " and port=" + this.dataNodeSelectorPort + ".");
 	}
 	
-	
-	public LocatedBlocks filterBlockLocations(LocatedBlocks blocks, String path, String username, String remoteAddress) {
-		LOG.info("Got decision request (" + path + ")!");
+	public LocatedBlocks filterBlockLocations(LocatedBlocks locatedBlocks, String path, String username, String remoteAddress) {
+		try {
+			LOG.info("Got decision request (" + path + ")!");
+			LOG.info("Request: " + toJson(locatedBlocks, path, username, remoteAddress));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// TODO: ask blackbox
-		return blocks;
+		return locatedBlocks;
+	}
+
+	public String toJson(LocatedBlocks locatedBlocks, String path, String username, String remoteAddress) throws IOException {
+	    final Map<String, Object> m = new TreeMap<String, Object>();
+	    m.put("locatedBlocks", JsonUtil.toJsonString(locatedBlocks));
+	    m.put("path", path);
+	    m.put("username", username);
+	    m.put("remoteAddress", remoteAddress);
+	    return JSON.toString(m);
 	}
 }
