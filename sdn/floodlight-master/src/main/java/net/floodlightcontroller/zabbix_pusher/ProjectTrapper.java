d736d6cce6f183dc0bb8199e7800f17536644b52
fubezz <fubezz.googlemail.com>
2014-06-08 02:20:24 +0200

    Changes at zabbixpusher

--- sdn/floodlight-master/src/main/java/net/floodlightcontroller/zabbix_pusher/ProjectTrapper.java
+++ sdn/floodlight-master/src/main/java/net/floodlightcontroller/zabbix_pusher/ProjectTrapper.java
@@ -8,16 +8,16 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,147 +28,114 @@ import java.util.List;
  * (created for Distributed Systems Project SoSe 2014 CIT TU Berlin)
  */
 public class ProjectTrapper{
-		
-	String command ="";
-	Process p = null;
-	
-	private String jsonMsg;
-	private OutputStream zabbixMsg;
 
-    public ProjectTrapper(){
-        zabbixMsg = new ByteArrayOutputStream();
-    }
-	
-	/**
-	 * Method for pushing desired metric information into specified zabbix monitor
-	 * 
-	 * @param zabbixServerIpAdress IP adress or DNS name of the zabbix monitor.
-	 * @param hostname Registered hostname within zabbix monitor (Warning: Not IP adress or DNS name). Also the name of the server to which the specified information should be assigned to.
-	 * @param itemKey Name of the item key for the metric information.(Casesensitive)
-	 * @param isKeyNumeric Boolean parameter for determining whether the numeric value is an integer or a character. (true = value is numeric, false = value is a character)
-	 * @param Value Metric information that is going to be sent to the monitor.
-	 */
-//	public void sendMetric(String zabbixServerIpAdress, String hostname,
-//			String itemKey,boolean isKeyNumeric, Object Value) {
-//
-//		//Constructing command for execution
-//		String command = "zabbix_sender -z " + zabbixServerIpAdress  +
-//				" -s " + hostname +
-//				" -k " +itemKey;
-//		if(isKeyNumeric){
-//			command += " -o " + Value.toString();
-//		}
-//		else{
-//			//TODO: Problem fixen mit dem Senden von Nachrichten mit Leerzeichen
-//			command += " -o \'" + (String)Value + "\'";
-//		}
-//
-//		System.out.println(command);
-//
-//		p = null;
-//
-//		//executing the commandline via Runtime
-//		try{
-//			p = Runtime.getRuntime().exec(command);
-////			p.waitFor();
-//		}
-//		catch(Exception e)
-//		{
-//			e.printStackTrace();
-//		}
-//
-//		//resetting the command
-//		command = "";
-//
-//	}
 	
 	public void sendMetricJson (String zabbixServerIpAdress, String hostname,
 	String itemKey,boolean isKeyNumeric, String value) throws IOException {
 
 	String zabbix_URL = "http://" + zabbixServerIpAdress + "/zabbix/api_jsonrpc.php";
-    String auth = userAuthenticate(zabbix_URL);
-    String keyUpdate = "\""+itemKey + "\":\"" + value + "\"";
-    String type = "\"type\":" + "\"2\"";
     String keyType;
     if (isKeyNumeric){
-       keyType = "\"value_type\":" + "\"0\"";
+       keyType = "0";
     }else{
-        keyType = "\"value_type\":" + "\"4\"";
+        keyType = "4";
     }
+       // System.err.println(value);
+        String sender = "{\n" +
+            //    "        \"jsonrpc\":\"2.0\",\n" +
+                "        \"request\":\"sender data\",\n" +
+                "        \"data\":[\n" +
+                "                {\n" +
+                "                        \"host\":\""+ hostname +"\",\n" +
+                "                        \"key\":\""+ itemKey + "\",\n" +
+                "                        \"value\":\""+ value +"\"\n" +
+            //    "                        \"value_type\":\""+ keyType+ "\"\n"+
+                "                 }\n" +
+                "       ]\n" +
+                "}";
+        int msglength = sender.length();
 
+        byte[] header = new byte[] {
+                'Z', 'B', 'X', 'D',
+                '\1',
+                (byte)(msglength & 0xFF),
+                (byte)((msglength >> 8) & 0x00FF),
+                (byte)((msglength >> 16) & 0x0000FF),
+                (byte)((msglength >> 24) & 0x000000FF),
+                '\0','\0','\0','\0'};
+        try{
+            Socket clientSocket = new Socket(zabbixServerIpAdress, 10051);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+//        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToServer.write(header);
+            outToServer.write(sender.getBytes());
+            outToServer.flush();
+            clientSocket.close();
+            outToServer.close();
+        } catch (UnknownHostException e){
+            System.err.println("Error while connecting to server");
+        } catch (IOException e){
+            System.err.println("Error while sending to Server");
 
-
-
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(zabbix_URL);
-        List<NameValuePair> valuePairList = new ArrayList<NameValuePair>();
-        valuePairList.add(new BasicNameValuePair("jsonrpc","2.0"));
-        valuePairList.add(new BasicNameValuePair("method","item.update"));
-        valuePairList.add(new BasicNameValuePair("params", "[" + keyUpdate + "," + type + "," + keyType + "]"));
-        valuePairList.add(new BasicNameValuePair("id","2"));
-        httpPost.setEntity(new UrlEncodedFormEntity(valuePairList));
-        CloseableHttpResponse response = httpclient.execute(httpPost);
-
-        System.out.println(response.getStatusLine());
-        response.close();
-		// --- prepare JSON string
-//		jsonMsg = "{"
-//		        + "\"request\":\"sender data\",\n"
-//		        + "\"data\":[\n"
-//		        +        "{\n"
-//		        +                "\"host\":\"" + hostname + "\",\n"
-//		        +                "\"key\":\"" + itemKey + "\",\n"
-//		        +                "\"value\":\"" + value.replace("\\", "\\\\") + "\"}]}\n" ;
-//
-//		System.out.println("\nJSON String output for debugging purposes:\n" +jsonMsg+ "\n");
-//
-//		// --- write JSON string to a zabbix message
-//		byte[] zabbixData = jsonMsg.getBytes();
-//		int lengthZabbixMsg = zabbixData.length;
-//        System.err.println(zabbixData.length);
-//        zabbixMsg.write(new byte[] {
-//			'Z', 'B', 'X', 'D',
-//			'\1',
-//			(byte)(lengthZabbixMsg & 0xFF),
-//			(byte)((lengthZabbixMsg >> 8) & 0x00FF),
-//			(byte)((lengthZabbixMsg >> 16) & 0x0000FF),
-//			(byte)((lengthZabbixMsg >> 24) & 0x000000FF),
-//			'\0','\0','\0','\0'});
-//
-//		zabbixMsg.write(zabbixData);
-	
-	}
-
-    private String userAuthenticate(String zabbix_url) throws IOException {
-
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(zabbix_url);
-        List<NameValuePair> valuePairList = new ArrayList<NameValuePair>();
-        valuePairList.add(new BasicNameValuePair("jsonrpc","2.0"));
-        valuePairList.add(new BasicNameValuePair("method","user.login"));
-        valuePairList.add(new BasicNameValuePair("params", "{\"user\": \"admin\", \"password\": \"zabbix\"}"));
-        valuePairList.add(new BasicNameValuePair("id","1"));
-        try {
-
-            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(valuePairList,"UTF-8");
-            System.err.println(EntityUtils.toString(entity,"UTF-8"));
-            httpPost.setEntity(entity);
-            httpPost.addHeader("Content-Type","application/json");
-            CloseableHttpResponse response = httpclient.execute(httpPost);
-            byte[] respArr = new byte[(int)response.getEntity().getContentLength()];
-            response.getEntity().getContent().read(respArr);
-            String out = new String(respArr);
-      //      System.err.println(out);
-
-            response.close();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         }
 
 
-
-        return null;
     }
+
+    /**
+     * This is an example to modify Zabbix from the Controller. Needed JSON and Apache HTTPClient Components
+      * @param zabbix_url
+     * @return
+     * @throws IOException
+     */
+//    private String userAuthenticate(String zabbix_url) throws IOException {
+//
+//        CloseableHttpClient httpclient = HttpClients.createDefault();
+//        HttpPost httpPost = new HttpPost(zabbix_url);
+//        List<NameValuePair> valuePairList = new ArrayList<NameValuePair>();
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("{\"jsonrpc\":\"2.0\"").
+//                append(",\"params\":{").
+//                append("\"user\":\"").append("admin").
+//                append("\",\"password\":\"").append("zabbix").
+//                append("\"},").
+//                append("\"method\":\"user.authenticate\",").
+//                append("\"id\":\"2\"}");
+//
+//        try {
+//
+//            httpPost.setEntity(new StringEntity(sb.toString()));
+//            httpPost.addHeader("Content-Type","application/json");
+//            CloseableHttpResponse response = httpclient.execute(httpPost);
+//            byte[] respArr = new byte[(int)response.getEntity().getContentLength()];
+//            response.getEntity().getContent().read(respArr);
+//            String out = new String(respArr);
+//          //  System.out.println(out);
+//            out = parseAuth(out);
+//            response.close();
+//            return out;
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    return null;
+//
+//    }
+//
+//    private String parseAuth(String out) {
+//        String res;
+//        String parts[] = out.split(":");
+//        for(int i = 0; i <parts.length ; i++){
+//            if (parts[i].endsWith("result\"")){
+//               // System.err.println(parts[i+1]);
+//                res = parts[i+1].replace("\"","");
+//                //System.err.println(res);
+//                return res.split(",")[0];
+//            }
+//        }
+//        return null;
+//    }
 
 }
 		
