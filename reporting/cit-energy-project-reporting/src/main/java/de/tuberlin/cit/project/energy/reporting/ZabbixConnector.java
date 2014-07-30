1db905187b9366d8a7acada8d03765e76c9e7415
Tobias Wollowski <tobias@jbbr.net>
2014-07-30 18:29:04 +0200

    * added power sum calculation for watt seconds

--- reporting/cit-energy-project-reporting/src/main/java/de/tuberlin/cit/project/energy/reporting/ZabbixConnector.java
+++ reporting/cit-energy-project-reporting/src/main/java/de/tuberlin/cit/project/energy/reporting/ZabbixConnector.java
@@ -1,5 +1,6 @@
 package de.tuberlin.cit.project.energy.reporting;
 
+import de.tuberlin.cit.project.energy.reporting.model.Power;
 import de.tuberlin.cit.project.energy.zabbix.ZabbixAPIClient;
 import de.tuberlin.cit.project.energy.zabbix.ZabbixParams;
 import de.tuberlin.cit.project.energy.zabbix.exception.InternalErrorException;
@@ -10,6 +11,8 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.naming.AuthenticationException;
 
 /**
@@ -18,7 +21,7 @@ import javax.naming.AuthenticationException;
  */
 public class ZabbixConnector {
 
-    private ZabbixAPIClient client;
+    private final ZabbixAPIClient client;
     static final String[] HOSTS = {"CitProjectOffice", "CitProjectAsok05"};
 
     public ZabbixConnector() throws KeyManagementException, NoSuchAlgorithmException {
@@ -26,26 +29,32 @@ public class ZabbixConnector {
 
     }
 
-    public ZabbixAPIClient getClient() {
-        return client;
-    }
 
-    public List<ZabbixHistoryObject> getPowerUsageForRange(long fromMillis, long toMillis) throws AuthenticationException, ExecutionException, IOException, IllegalArgumentException, InternalErrorException, InterruptedException, KeyManagementException, NoSuchAlgorithmException {
+    protected List<ZabbixHistoryObject> getPowerUsageForRange(String hostname, long fromMillis, long toMillis) throws AuthenticationException, ExecutionException, IOException, IllegalArgumentException, InternalErrorException, InterruptedException, KeyManagementException, NoSuchAlgorithmException {
         List<ZabbixHistoryObject> result = null;
-        ZabbixConnector connector = new ZabbixConnector();
-
-        result = connector.getClient().getNumericHistory(HOSTS[0], "datanode.power", fromMillis, toMillis);
-
+        result = client.getNumericHistory(hostname, "datanode.power", fromMillis, toMillis);
         return result;
     }
 
-//    @Override
+    public Power getPower(String hostname, long from, long to) {
+        List<ZabbixHistoryObject> historyObjects = null;
+        try {
+            historyObjects = getPowerUsageForRange(hostname, from, to);
+        } catch (AuthenticationException | ExecutionException | IOException | IllegalArgumentException | InternalErrorException | InterruptedException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(ZabbixConnector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (historyObjects != null) {
+            return new Power(historyObjects);
+        }
+        return null;
+    }
+
     public String getUserTraffic(String userName, long fromMillis, long toMillis) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 //    @Override
-    public String getUserStorage(String userName, long fromMillis, long toMillis) {
+    public String getUserStorageAverage(String userName, long fromMillis, long toMillis) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
--- reporting/cit-energy-project-reporting/src/main/java/de/tuberlin/cit/project/energy/reporting/model/Power.java
+++ reporting/cit-energy-project-reporting/src/main/java/de/tuberlin/cit/project/energy/reporting/model/Power.java
@@ -1,5 +1,9 @@
 package de.tuberlin.cit.project.energy.reporting.model;
 
+import de.tuberlin.cit.project.energy.zabbix.model.ZabbixHistoryObject;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * represents power in kwh used for a specific range.
  *
@@ -7,14 +11,57 @@ package de.tuberlin.cit.project.energy.reporting.model;
  */
 public class Power {
 
-    private final double powerKwh;
+    private final List<PowerValue> powerValues;
 
-    public Power(double powerKwh) {
-        this.powerKwh = powerKwh;
+    public Power(List<ZabbixHistoryObject> response) {
+        powerValues = new ArrayList<>();
+        for (ZabbixHistoryObject zabbixHistoryObject : response) {
+            powerValues.add(new PowerValue(zabbixHistoryObject.getClock(), zabbixHistoryObject.getIntValue()));
+        }
     }
 
-    public double getPowerKwh() {
-        return powerKwh;
+    /**
+     * converts wattseconds to kwh. dividing given value through 3600000.
+     *
+     * @param wattSeconds
+     * @return
+     */
+    public static Double wsToKwh(Double wattSeconds) {
+        return wattSeconds / 3600000;
+    }
+
+    public Double getPowerAsWattSeconds(long startTimeMillis, long endTimeMillis) {
+        // use seconds for times
+        long lastTimeSeconds = startTimeMillis / 1000;
+        long stopTimeSeconds = endTimeMillis / 1000;
+        double wattSeconds = 0.0;
+        float value;
+        if (powerValues != null) {
+            for (PowerValue pv : powerValues) {
+                if (pv.timeAsSeconds < stopTimeSeconds) {
+                    value = pv.powerAsWatt;
+                    wattSeconds += (pv.timeAsSeconds - lastTimeSeconds) * value;
+                    lastTimeSeconds = pv.timeAsSeconds;
+                } else {
+                    // will ignore last two seconds...
+                    break;
+                }
+            }
+            return wattSeconds;
+        }
+        return null;
+    }
+
+    public class PowerValue {
+
+        public long timeAsSeconds;
+        public int powerAsWatt;
+
+        public PowerValue(Long timeAsSeconds, int powerAsWatt) {
+            this.timeAsSeconds = timeAsSeconds.intValue();
+            this.powerAsWatt = powerAsWatt;
+        }
+
     }
 
 }
